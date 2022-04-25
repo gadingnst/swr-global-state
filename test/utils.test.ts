2904efe4306a1b0ac508a1c4189fc85e0e7502cb
Sutan Gading Nasution <contact@sutanlab.id>
2022-04-26 01:56:00 +0700

    test: write test for hasValue function()

---
+++ test/utils.test.ts
@@ -0,0 +1,14 @@
+import { hasValue } from '../src';
+
+describe('Utility test', () => {
+  describe('hasValue() function', () => {
+    it('should return true if variable has value', () => {
+      expect(hasValue(1)).toBe(true);
+      expect(hasValue('')).toBe(true);
+      expect(hasValue(0)).toBe(true);
+      expect(hasValue(NaN)).toBe(true);
+      expect(hasValue(null)).toBe(false);
+      expect(hasValue(undefined)).toBe(false);
+    });
+  });
+});
