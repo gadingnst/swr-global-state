04ce655198bc01379f5b7dcdd3652fa3ff20d3ac
Gading Nasution <sutan.gnst@gmail.com>
2022-10-08 12:07:30 +0700

    refactor: upgrade

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import useSWR, { Key, KeyedMutator, useSWRConfig } from 'swr';
 
 export interface Store<T> {
@@ -10,25 +10,25 @@ export interface Store<T> {
 type MutatorCallback = <T>(state: T) => T;
 
 /**
- *
+ * check is window is support local storage
  * @param persist
- * @returns boolean if cache is supported
+ * @returns {boolean} boolean if cache is supported
  */
-const isSupportCache = (persist?: boolean): boolean =>
+const isSupportPersistance = (persist?: boolean): boolean =>
   window.localStorage && !!persist;
 
 /**
- *
+ * check is variable has value
  * @param variable
- * @returns boolean if variable is has value
+ * @returns {boolean} boolean if variable is has value
  */
 export const hasValue = <T>(variable: T): boolean =>
   (typeof variable !== 'undefined' && variable !== null);
 
 /**
- *
+ * get cached value from local storage
  * @param key store key
- * @returns parsed persisted state
+ * @returns {T} parsed persisted state
  */
 export const getCache = <T = any>(key: Key): T => {
   const cache = window.localStorage.getItem(key as string) ?? 'null';
@@ -40,12 +40,12 @@ export const getCache = <T = any>(key: Key): T => {
 };
 
 /**
- *
+ * cache the value into local storage
  * @param key store key
  * @param value to be cached
- * @returns void
+ * @returns {void}
  */
-export const setCache = <T>(key: Key, value: T) => {
+export const setCache = <T>(key: Key, value: T): void => {
   if (hasValue(value)) {
     const data = JSON.stringify(value);
     window.localStorage.setItem(key as string, data);
@@ -55,33 +55,37 @@ export const setCache = <T>(key: Key, value: T) => {
 };
 
 /**
- *
+ * using global state with SWR
  * @param data state that to be shared or cached
- * @returns state and setter
+ * @returns {readonly [T, KeyedMutator<T>]} state and setter
  */
-export function useStore<T>(data: Store<T>) {
+export function useStore<T>(data: Store<T>): readonly [T, KeyedMutator<T>] {
   const { key, initial, persist } = data;
   const { cache } = useSWRConfig();
   const { data: state, mutate } = useSWR<T>(key, {
     fallbackData: initial ?? cache.get(key)
   });
 
-  const setState: KeyedMutator<T> = (...args) => {
+  const setState: KeyedMutator<T> = useCallback((...args) => {
     const [mutator, ...otherArgs] = args;
     const newState = typeof mutator === 'function'
       ? (mutator as MutatorCallback)(state as T)
       : mutator;
-    if (isSupportCache(persist)) {
+    if (isSupportPersistance(persist)) {
       setCache(key, newState);
     }
     return mutate(newState, ...otherArgs);
-  };
+  }, [key, state, persist]);
 
   useEffect(() => {
-    if (isSupportCache(persist)) {
+    const subscribed = useRef(true);
+    if (subscribed.current && isSupportPersistance(persist)) {
       const persistState = getCache<T>(key);
       setState(persistState ?? initial);
     }
+    return () => {
+      subscribed.current = false;
+    };
   }, []);
 
   return [state as T, setState] as const;
